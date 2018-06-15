9d2b4e9fcac2fb953f7625e56e61f6443229ef0e
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-06-15 10:11:21 -0300

    Criando um Modelo em JS e usando ele no index.html

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Negociacao{
+    
+    
+    
+    constructor(){
+        this.data = new Date();
+        this.quantidade = 1;
+        this.valor = 0.0;
+    }
+}
