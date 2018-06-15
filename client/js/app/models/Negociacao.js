073ba950d45c25b776c3290ecf0112027c9f030f
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-06-15 10:27:04 -0300

    Criando mesmo modelo pelo construtor e tornando atributos privados (hj somente por convencao colocando um _ a frente do atributo)

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -5,12 +5,29 @@
  */
 
 class Negociacao{
-    
-    
-    
-    constructor(){
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+
+    constructor(data, quantidade, valor){
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
+    
+    getVolume(){
+        return this._quantidade * this._valor;
+    }
+    
+    getData(){
+        return this._data;
+    }
+    
+    getQuantidade(){
+        return this._quantidade;
+    }
+    
+    getValor(){
+        return this._valor;
+    }
+    
+    
+    
 }
