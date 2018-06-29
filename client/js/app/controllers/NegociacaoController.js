1c8fd27623a7dfca004c5281ed86f9b0738c9080
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-06-29 10:05:21 -0300

    Criando um Controller e chamando uma funcao do controller ao clicar em um botao na view

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class NegociacaoController{
+    
+    adiciona(event){
+        event.preventDefault(); // tira o comportamento default do botao
+        alert('Chamei ação no controller!');
+    }
+}
--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -12,23 +12,23 @@ class Negociacao{
         this._valor = valor;
         Object.freeze(this);//congela o objeto para nao poder ser alterado
     }
-    
+
     get volume(){
         return this._quantidade * this._valor;
     }
-    
+
     get data(){
-        return new Date(this._data.getTime());
+        return new Date(this._data.getTime()); //se retornasemos this_data o programador conseguiria quebrar o encapsulamento ao inves disso retornamos para ele uma nova referencia para data, assim se ele der uma de esperto e alterar lah no código, não ira impactar aqui dentro do objeto
     }
-    
+
     get quantidade(){
         return this._quantidade;
     }
-    
+
     get valor(){
         return this._valor;
     }
-    
-    
-    
+
+
+
 }
