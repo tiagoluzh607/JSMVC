1780858dccf3f023ace4747533ce6a7ee788ee98
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-06-29 14:07:23 -0300

    Criando no crontroller um model Negociacao

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,8 +6,36 @@
 
 class NegociacaoController{
     
+    
+    constructor(){
+        
+        let $ = document.querySelector.bind(document); //criando um atalho para chamar o queryselector com $
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+            
+    }
+    
+    
     adiciona(event){
         event.preventDefault(); // tira o comportamento default do botao
-        alert('Chamei ação no controller!');
+        
+        let data = new Date(
+                ...this._inputData.value // os tres pontos quer dizer que este array de tres posicoes cada posicao vai para um parametro do construtor de date
+                .split('-')
+                .map((item, indice) => item - indice % 2) //Arrow  function já faz o retorno automatico
+                
+        );
+        
+       let negociacao = new Negociacao(
+            //new Date (this._inputData.value.replace(/-/g,',')), // a data esta em formato Date porem do html vem uma string nesse formato yyyy-mm-dd e para criarmos uma data precisamos subtituir "-" por "," para usar no construtor de date
+            data,   
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+        
+        console.log(negociacao);
+        
+        
     }
 }
