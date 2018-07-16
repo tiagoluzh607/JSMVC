b5aba9a3d596884e234fde4f7d8c303cbd3b2c42
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-16 09:50:58 -0300

    limpando formulario ao inserir negociacao refatorando a logica de inserir negociacao

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -19,20 +19,31 @@ class NegociacaoController{
     
     adiciona(event){
         event.preventDefault(); // tira o comportamento default do botao
-        
-        
-       let negociacao = new Negociacao(
-            //new Date (this._inputData.value.replace(/-/g,',')), // a data esta em formato Date porem do html vem uma string nesse formato yyyy-mm-dd e para criarmos uma data precisamos subtituir "-" por "," para usar no construtor de date
-            DateHelper.textoParaData(this._inputData.value),   
-            this._inputQuantidade.value,
-            this._inputValor.value
-        );
-        
-        this._listaNegociacoes.adiciona(negociacao);
+                
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
         
         console.log(this._listaNegociacoes.negociacoes);
         
         //console.log(DateHelper.dataParaTexto(negociacao.data));
         
+    }
+    
+    _criaNegociacao(){
+        
+        return new Negociacao(
+            //new Date (this._inputData.value.replace(/-/g,',')), // a data esta em formato Date porem do html vem uma string nesse formato yyyy-mm-dd e para criarmos uma data precisamos subtituir "-" por "," para usar no construtor de date
+            DateHelper.textoParaData(this._inputData.value),   
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    }
+    
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        
+        this._inputData.focus();
     }
 }
